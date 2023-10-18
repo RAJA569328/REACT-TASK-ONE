7bb0552672528285efe9ccc2460601487fd9f367
RAJA569328 <rajaramanathan263@gmail.com>
2023-10-18 11:37:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import Card from "./Card.jsx";
+function App() {
+  const X = [
+    {
+      plan: "FREE",
+      price: "0",
+      userEnabler:true,
+      user: "Single User",
+      Storage: "50GB storage",
+      StorageEnabler:true,
+      publicProjects: "unlimited Public Projects",
+      publicProjectsEnabler:true,
+      communityAccess: "Community Access",
+      communityAccessEnabler:true,
+      privateProjects: "Unlimited Private Projects",
+      privateProjectsEnabler:false,
+      Support: "Dedicated Phone Support",
+      SupportEnabler:false,
+      SubDomain: "Free SubDomain",
+      SubDomainEnabler:false,
+      Report: "Monthly Status Reports",
+      ReportEnabler:false,
+    },
+    {
+      plan: "PLUS",
+      price: "9",
+      user: "5 User",
+      userbold:true,
+      userEnabler:true,
+      Storage: "50GB storage",
+      StorageEnabler:true,
+      publicProjects: "Unlimited Public Projects",
+      publicProjectsEnabler:true,
+      communityAccess: "Community Access",
+      communityAccessEnabler:true,
+      privateProjects: "Unlimited Private Projects",
+      privateProjectsEnabler:true,
+      Support: "Dedicated Phone Support",
+      SupportEnabler:true,
+      SubDomain: "Free SubDomain",
+      SubDomainbold:false,
+      SubDomainEnabler:true,
+      Report: "Monthly Status Reports",
+      ReportEnabler:false,
+    },
+    {
+      plan: "PRO",
+      price: "49",
+      user: "Unlimited User",
+      userbold:true,
+      userEnabler:true,
+      Storage: "5GB storage",
+      StorageEnabler:true,
+      publicProjects: "Unlimited Public Projects",
+      publicProjectsEnabler:true,
+      communityAccess: "Community Access",
+      communityAccessEnabler:true,
+      privateProjects: "Unlimited Private Projects",
+      privateProjectsEnabler:true,
+      Support: "Dedicated Phone Support",
+      SupportEnabler:true,
+      SubDomain: "Free SubDomain",
+      SubDomainbold:true,
+      SubDomainEnabler:true,
+      Report: "Monthly Status Reports",
+      ReportEnabler:true,
+    },
+  ];
+  return (
+    <>
+      <section className="pricing py-5">
+        <div className="container">
+          <div className="row">
+            {X.map((y, i) => {
+              return <Card X={y} key={i} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Card(props) {
+    let value = props.X
+  return <>
+  <div className="col-lg-4">
+          <div className="card mb-5 mb-lg-0">
+            <div className="card-body">
+              <h5 className="card-title text-muted text-uppercase text-center">{value.plan}</h5>
+              <h6 className="card-price text-center">${value.price}<span className="period">/month</span></h6>
+              <hr/>
+              <ul className="fa-ul">
+                <li className={value.userEnabler?"":"text-muted"}><span className="fa-li"><i className={value.userEnabler?"fas fa-check":"fas fa-times"}></i></span>{value.user}</li>
+                <li className={value.StorageEnabler?"":"text-muted"}><span className="fa-li"><i className={value.StorageEnabler?"fas fa-check":"fas fa-times"}></i></span>{value.Storage}</li>
+                <li className={value.publicProjectsEnabler?"":"text-muted"}><span className="fa-li"><i className={value.publicProjectsEnabler?"fas fa-check":"fas fa-times"}></i></span>{value.publicProjects}</li>
+                <li className={value.communityAccessEnabler?"":"text-muted"}><span className="fa-li"><i className={value.communityAccessEnabler?"fas fa-check":"fas fa-times"}></i></span>{value.communityAccess}</li>
+                <li className={value.privateProjectsEnabler?"":"text-muted"}><span className="fa-li"><i className={value.privateProjectsEnabler?"fas fa-check":"fas fa-times"}></i></span>{value.privateProjects}</li>
+                <li className={value.SupportEnabler?"":"text-muted"}><span className="fa-li"><i className={value.SupportEnabler?"fas fa-check":"fas fa-times"}></i></span>{value.Support}</li>
+                <li className={value.SubDomainEnabler?"":"text-muted"}><span className="fa-li"><i className={value.SubDomainEnabler?"fas fa-check":"fas fa-times"}></i></span>{value.SubDomain}</li>
+                <li className={value.ReportEnabler?"":"text-muted"}><span className="fa-li"><i className={value.ReportEnabler?"fas fa-check":"fas fa-times"}></i></span>{value.Report}</li>
+              </ul>
+              <div className="d-grid">
+                <a href="#" className="btn btn-primary text-uppercase">Button</a>
+              </div>
+            </div>
+          </div>
+        </div>
+  </>
+}
+
+export default Card
